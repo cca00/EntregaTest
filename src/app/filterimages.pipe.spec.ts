098670252ace1172fa3cbcdbf04bd1a6b247ca5f
Cristhian Chica <cc.acosta00@gmail.com>
2021-03-08 15:11:37 -0500

    otro caso de prueba y correción

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -19,7 +19,7 @@ describe('FilterimagesPipe', () => {
 });
 
 describe('Filtrado de Imagenes', () => {
-  it('Si da clic en "Perro" debe filtrar todos los perros',()=>{
+  it('Si da clic en "Perro" debe filtrar todos los gatos',()=>{
     const pipe = new FilterimagesPipe();
     const imagesPerros = [  { 
       "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
@@ -27,11 +27,15 @@ describe('Filtrado de Imagenes', () => {
     { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }];
     expect(pipe.transform(Imagesdelatils, 'perro')).toEqual(imagesPerros);
   });
-  it('Si da clic en "Perro" debe filtrar todos los gatos',()=>{
+  it('Si da clic en "Perro" debe filtrar todos los perros',()=>{
     const pipe = new FilterimagesPipe();
     const imagesGatos = [
     { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
     { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" }];
     expect(pipe.transform(Imagesdelatils, 'gato')).toEqual(imagesGatos);
+  });
+  it('Si da clic en "All" debe filtrar todas las imagenes',()=>{
+    const pipe = new FilterimagesPipe();
+    expect(pipe.transform(Imagesdelatils, 'all')).toEqual(Imagesdelatils);
   });
 });
